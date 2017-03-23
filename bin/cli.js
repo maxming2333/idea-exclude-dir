762ff52487e648414ba811d7efb771fba8672f27
龙瞎 <yonghua.myh@alibaba-inc.com>
2017-03-23 21:59:10 +0800

    fix: 增加判断

--- bin/cli.js
+++ bin/cli.js
@@ -3,12 +3,27 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs');
 const exclude = require('..');
 const CWD = process.cwd();
-const pkg = require(path.join(CWD, 'package.json'));
 const argv = require('minimist')(process.argv.slice(2));
-const pkgList = pkg.config && pkg.config.idea && pkg.config.idea.index;
+const pkgFile = path.join(CWD, 'package.json');
+const modulesFile = path.join(CWD, '.idea', 'modules.xml');
 let list = ['/node_modules'];
+let pkgList;
+
+if (!fs.existsSync(modulesFile)) {
+  process.exit(0);
+}
+
+// 检测 package.json
+if (fs.existsSync(pkgFile)){
+  const pkg = require(path.join(CWD, 'package.json'));
+  pkgList = pkg.config && pkg.config.idea && pkg.config.idea.index;
+} else {
+  process.exit(0);
+}
+
 
 if (pkgList) {
   list = list.concat(pkgList);
